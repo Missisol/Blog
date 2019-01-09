3402518f0f911a6ec843f28c3b98580a412e59a4
missisol <missisololol@gmail.com>
2019-01-09 16:43:40 +0300

    Update itemsMenu.js.

--- src/source/itemsMenu.js
+++ src/source/itemsMenu.js
@@ -2,7 +2,4 @@ export default [
     { href: '/', title: 'Home' },
     { href: '/posts', title: 'Posts' },
     { href: '/users', title: 'Users' },
-    // { href: '/comments', title: 'Комментарии' },
-    // { href: '/messages', title: 'Чат' },
-    // { href: '/user', title: 'Войти в чат' },
 ]
